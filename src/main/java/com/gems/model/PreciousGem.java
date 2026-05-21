a375bc248aeb420216285bc38ed36659cc9991ab
aleksandra08052008-cmyk <aleksandra08052008@gmail.com>
2026-05-21 18:05:20 +0400

    Create PreciousGem.java

---
+++ src/main/java/com/gems/model/PreciousGem.java
@@ -0,0 +1,11 @@
+package com.gems.model;
+
+/**
+ * Дорогоцінне каміння
+ */
+public class PreciousGem extends Gem {
+
+    public PreciousGem(String name, double caratWeight, double pricePerCarat, double transparency) {
+        super(name, caratWeight, pricePerCarat, transparency);
+    }
+}
