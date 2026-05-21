468d8df44d5c7857060e388d7cae99519f46a38e
aleksandra08052008-cmyk <aleksandra08052008@gmail.com>
2026-05-21 18:05:54 +0400

    Create SemiPreciousGem.java

---
+++ src/main/java/com/gems/model/SemiPreciousGem.java
@@ -0,0 +1,11 @@
+package com.gems.model;
+
+/**
+ * Напівдорогоцінне каміння
+ */
+public class SemiPreciousGem extends Gem {
+
+    public SemiPreciousGem(String name, double caratWeight, double pricePerCarat, double transparency) {
+        super(name, caratWeight, pricePerCarat, transparency);
+    }
+}
